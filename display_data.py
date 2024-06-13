2d25d57c3705e85c988303deea87c04468f49001
gigaSec <105108954+giga-sec@users.noreply.github.com>
2024-06-13 11:50:24 +0800

    Update display_data.py

--- display_data.py
+++ display_data.py
@@ -6,7 +6,13 @@ import plotly.io as pio
 import openai
 import nltk
 
-nltk.download('wordnet')
+nltk_data_dir = "./resources/nltk_data_dir/"
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.clear()
+nltk.data.path.append(nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir)
 
 
 pio.templates.default = "plotly_dark"  # You can choose from "plotly", "plotly_white", "ggplot2", "seaborn", etc.
